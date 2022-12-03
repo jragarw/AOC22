22fa4e61fad8229ba6f319bed6006589d2807598
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-03 17:42:37 +0000

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+filename = 'AOC22-D1-input.txt'
+
+
+with open(filename) as f:
+    data = [list(map(int, row.split())) for row in f.read().split('\n\n')]
+
+dataframe = pd.DataFrame(data)
