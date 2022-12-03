72d9f5830ec8d79f1d7721585e8031666873808a
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-03 21:18:42 +0000

    Update day1.py

--- day1.py
+++ day1.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 filename = 'AOC22-D1-input.txt'
 
-
 with open(filename) as f:
     data = [list(map(int, row.split())) for row in f.read().split('\n\n')]
 
@@ -10,4 +9,5 @@ dataframe = pd.DataFrame(data)
 
 dataframe.loc[:,'Total'] = dataframe.sum(axis=1)
 
-print(dataframe)
+max_element = df['Total'].max()
+print(max_element)
