8491a07754887ccbacfbad1ddda1213dd3b646aa
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 02:44:09 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,20 +1,22 @@
 import pandas as pd
 import numpy as np
 
+#   A = Rock        X = Rock
+#   B = Paper       Y = Paper
+#   C = Scissors    Z = Scissors
+
+
 filename = 'AOC22-D2-input.txt'
 
 df = pd.read_csv(filename, sep=' ', header = None)
 
 df.set_axis(['Opponent', 'Response'], axis=1)
 
-conditions = [
-    df.loc[df['Opponent'] == 'A' & df.loc[df['Response'] == 'Y', 'Result'] = 6
-    df.loc[df['Opponent'] == 'B' & df.loc[df['Response'] == 'Z', 'Result'] = 6 
-    df.loc[df['Opponent'] == 'C' & df.loc[df['Response'] == 'Y', 'Result'] = 6
-    df.loc[df['Opponent'] == 'B' & df.loc[df['Response'] == 'Y', 'Result'] = 6
-    df.loc[df['Opponent'] == 'C' & df.loc[df['Response'] == 'Z', 'Result'] = 6
-           
-df['result'] = np.select(conditions, choices, default='0')
+    df.loc[(df['Opponent'] == 'A') & ([df['Response'] == 'Y'), 'Result'] = '6'
+    df.loc[(df['Opponent'] == 'B') & ([df['Response'] == 'Z'), 'Result'] = '6'
+    df.loc[(df['Opponent'] == 'C') & ([df['Response'] == 'X'), 'Result'] = '6'
 
-
+                                      
+df.loc[df['Result'].isnull(), 'Result'] = 0
+                                      
 print(df)
