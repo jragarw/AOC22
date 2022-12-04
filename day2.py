f85acb19954c2f3cea9f4aaa3fafdc42971e4d48
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 02:26:24 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -6,11 +6,11 @@ filename = 'AOC22-D2-input.txt'
 df = pd.read_csv(filename, sep=' ', header = None)
 
 conditions = [
-    (df.loc[df['0'] == 'A') & (df.loc[df['1'] == 'Y'),
-    (df.loc[df['0'] == 'B') & (df.loc[df['1'] == 'Z'),
-    (df.loc[df['0'] == 'C') & (df.loc[df['1'] == 'Y'),
-    (df.loc[df['0'] == 'B') & (df.loc[df['1'] == 'Y'),
-    (df.loc[df['0'] == 'C') & (df.loc[df['1'] == 'Z'),
+    df.loc[df['0'] == 'A' & df.loc[df['1'] == 'Y',
+    df.loc[df['0'] == 'B' & df.loc[df['1'] == 'Z',
+    df.loc[df['0'] == 'C' & df.loc[df['1'] == 'Y',
+    df.loc[df['0'] == 'B' & df.loc[df['1'] == 'Y',
+    df.loc[df['0'] == 'C' & df.loc[df['1'] == 'Z',
 ]
 
 choices = ['6','6','6','3','3','3']
