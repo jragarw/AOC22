963f80f70869e79c1f6885aa4cddd735eee9d67f
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 02:44:45 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -12,9 +12,9 @@ df = pd.read_csv(filename, sep=' ', header = None)
 
 df.set_axis(['Opponent', 'Response'], axis=1)
 
-    df.loc[(df['Opponent'] == 'A') & ([df['Response'] == 'Y'), 'Result'] = '6'
-    df.loc[(df['Opponent'] == 'B') & ([df['Response'] == 'Z'), 'Result'] = '6'
-    df.loc[(df['Opponent'] == 'C') & ([df['Response'] == 'X'), 'Result'] = '6'
+df.loc[(df['Opponent'] == 'A') & ([df['Response'] == 'Y'), 'Result'] = '6'
+df.loc[(df['Opponent'] == 'B') & ([df['Response'] == 'Z'), 'Result'] = '6'
+df.loc[(df['Opponent'] == 'C') & ([df['Response'] == 'X'), 'Result'] = '6'
 
                                       
 df.loc[df['Result'].isnull(), 'Result'] = 0
