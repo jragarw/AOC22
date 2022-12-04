0cb21f5d983695b26ce9ee37bed1281fbed544cb
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 14:53:14 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-#   A = Rock        X = Rock
-#   B = Paper       Y = Paper
-#   C = Scissors    Z = Scissors
+#   A = Rock        X = Rock     1   X = LOSE
+#   B = Paper       Y = Paper    2   Y = DRAW
+#   C = Scissors    Z = Scissors 3   Z = WIN
 
 
 filename = 'AOC22-D2-input.txt'
@@ -12,16 +12,20 @@ df = pd.read_csv(filename, sep=' ', header = None)
 
 df.set_axis(['Opponent', 'Response'], axis=1)
 
-df.loc[(df['Opponent'] == 'A') & (df['Response'] == 'Y'), 'Result'] = 6
-df.loc[(df['Opponent'] == 'B') & (df['Response'] == 'Z'), 'Result'] = 6
-df.loc[(df['Opponent'] == 'C') & (df['Response'] == 'X'), 'Result'] = 6
 df.loc[(df['Opponent'] == 'A') & (df['Response'] == 'X'), 'Result'] = 3
-df.loc[(df['Opponent'] == 'B') & (df['Response'] == 'Y'), 'Result'] = 3
-df.loc[(df['Opponent'] == 'C') & (df['Response'] == 'Z'), 'Result'] = 3
+df.loc[(df['Opponent'] == 'A') & (df['Response'] == 'Y'), 'Result'] = 1
+df.loc[(df['Opponent'] == 'A') & (df['Response'] == 'Z'), 'Result'] = 2
+df.loc[(df['Opponent'] == 'B') & (df['Response'] == 'X'), 'Result'] = 1
+df.loc[(df['Opponent'] == 'B') & (df['Response'] == 'Y'), 'Result'] = 2
+df.loc[(df['Opponent'] == 'B') & (df['Response'] == 'Z'), 'Result'] = 3
+df.loc[(df['Opponent'] == 'C') & (df['Response'] == 'X'), 'Result'] = 2
+df.loc[(df['Opponent'] == 'C') & (df['Response'] == 'Y'), 'Result'] = 3
+df.loc[(df['Opponent'] == 'C') & (df['Response'] == 'Z'), 'Result'] = 1
 
-df.loc[(df['Response'] == 'X'), 'ThrowPoints'] = 1
-df.loc[(df['Response'] == 'Y'), 'ThrowPoints'] = 2
-df.loc[(df['Response'] == 'Z'), 'ThrowPoints'] = 3
+
+df.loc[(df['Response'] == 'X'), 'ThrowPoints'] = 0
+df.loc[(df['Response'] == 'Y'), 'ThrowPoints'] = 3
+df.loc[(df['Response'] == 'Z'), 'ThrowPoints'] = 6
 
 df.loc[df['Result'].isnull(), 'Result'] = 0
 
