1ed20b6348b5114ae58e6dc99a492bddfe9f0809
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 16:21:27 +0000

    Update day3.py

--- day3.py
+++ day3.py
@@ -2,6 +2,10 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 import pandas as pd
+import time
+
+# get the start time
+st = time.time()
 
 filename = 'AOC22-D3-input.txt'
 
@@ -15,3 +19,11 @@ df['BackpackCompLen'] = df['BackpackFull'].apply(len).div(2)
 
 
 print(df)
+
+
+# get the end time
+et = time.time()
+
+# get the execution time
+elapsed_time = et - st
+print('Execution time:', elapsed_time, 'seconds')
