78dd55373b5d0502688595bb0249d1d55f5a5e81
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 15:46:31 +0000

    Update day3.py

--- day3.py
+++ day3.py
@@ -6,7 +6,7 @@ df = pd.read_csv(filename, sep='/n', header = None)
 
 df.set_axis(['BackpackFull'], axis=1)
 
-df['Quarters_length'] = df['Quarters'].apply(len)
+df['BackpackLength'] = df['BackpackFull'].apply(len)
 
 
 
