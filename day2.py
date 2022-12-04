ed5e8ff0d8a7dbcd3c7562d025969cba50d6afc8
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 02:48:48 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -15,7 +15,14 @@ df.set_axis(['Opponent', 'Response'], axis=1)
 df.loc[(df['Opponent'] == 'A') & (df['Response'] == 'Y'), 'Result'] = '6'
 df.loc[(df['Opponent'] == 'B') & (df['Response'] == 'Z'), 'Result'] = '6'
 df.loc[(df['Opponent'] == 'C') & (df['Response'] == 'X'), 'Result'] = '6'
-                                      
+df.loc[(df['Opponent'] == 'A') & (df['Response'] == 'X'), 'Result'] = '3'
+df.loc[(df['Opponent'] == 'B') & (df['Response'] == 'Y'), 'Result'] = '3'
+df.loc[(df['Opponent'] == 'C') & (df['Response'] == 'Z'), 'Result'] = '3'
+
+df.loc[(df['Response'] == 'X'), 'ThrowPoints'] = '1'
+df.loc[(df['Response'] == 'Y'), 'ThrowPoints'] = '2'
+df.loc[(df['Response'] == 'Z'), 'ThrowPoints'] = '3'
+
 df.loc[df['Result'].isnull(), 'Result'] = 0
                                       
 print(df)
