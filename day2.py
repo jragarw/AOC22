a0009c659442f0a8373eb4f8f83bb4ccc353d24b
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 02:34:50 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -8,15 +8,12 @@ df = pd.read_csv(filename, sep=' ', header = None)
 df.set_axis(['Opponent', 'Response'], axis=1)
 
 conditions = [
-    df.loc[df['Opponent'] == 'A' & df.loc[df['Response'] == 'Y',
-    df.loc[df['Opponent'] == 'B' & df.loc[df['Response'] == 'Z',
-    df.loc[df['Opponent'] == 'C' & df.loc[df['Response'] == 'Y',
-    df.loc[df['Opponent'] == 'B' & df.loc[df['Response'] == 'Y',
-    df.loc[df['Opponent'] == 'C' & df.loc[df['Response'] == 'Z',
-]
-
-choices = ['6','6','6','3','3','3']
-
+    df.loc[df['Opponent'] == 'A' & df.loc[df['Response'] == 'Y', 'Result'] = '6' 
+    df.loc[df['Opponent'] == 'B' & df.loc[df['Response'] == 'Z', 'Result'] = '6' 
+    df.loc[df['Opponent'] == 'C' & df.loc[df['Response'] == 'Y', 'Result'] = '6' 
+    df.loc[df['Opponent'] == 'B' & df.loc[df['Response'] == 'Y', 'Result'] = '6' 
+    df.loc[df['Opponent'] == 'C' & df.loc[df['Response'] == 'Z', 'Result'] = '6' 
+           
 df['result'] = np.select(conditions, choices, default='0')
 
 
