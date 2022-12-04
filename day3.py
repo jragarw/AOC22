3973b1890f0c4d1a07b7787678468230ca39ea96
jragarw <86162381+jragarw@users.noreply.github.com>
2022-12-04 15:19:43 +0000

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,8 +1,8 @@
-import pandas
+import pandas as pd
 
 filename = 'AOC22-D3-input.txt'
 
-df = pd.read_csv(filename, sep=' ', header = None)
+df = pd.read_csv(filename, sep='/n', header = None)
 
 df.set_axis(['BackpackFull'], axis=1)
 
